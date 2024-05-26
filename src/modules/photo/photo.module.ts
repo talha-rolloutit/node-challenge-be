c7e96dd6261a9377377d4ed9439dcf55a3964ef3
talha-rolloutit <talha.waheed6618@gmail.com>
2024-05-26 21:28:39 +0500

    feat: bonus task
    add job schedular to add new uploaded images after 1 hour

--- src/modules/photo/photo.module.ts
+++ src/modules/photo/photo.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { FlickrService } from './flickr.service';
 import { PhotoController } from './photo.controller';
 import { PhotoService } from './photo.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
+  imports: [ScheduleModule.forRoot()],
   controllers: [PhotoController],
   providers: [FlickrService, PhotoService],
   exports: [FlickrService, PhotoService],
